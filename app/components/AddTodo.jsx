e0b60d2956c52aee7a3d02284b026213c3a0de75
Mladen <mladja92@yahoo.com>
2017-03-04 00:28:51 +0100

    style todo app

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -14,7 +14,7 @@ const AddTodo = React.createClass({
   },
   render: function () {
     return (
-      <div>
+      <div className="container__footer">
         <form onSubmit={this.onSubmit}>
           <input type="text" ref="todoText" placeholder="What do you need to do?"/>
           <button className="button expanded">Submit</button>
--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -4,6 +4,7 @@ const moment = require('moment');
 const Todo = React.createClass({
   render: function () {
     const {text, id, completed, createdAt, completedAt} = this.props;
+    const todoClassName = completed ? 'todo todo-completed' : 'todo';
     const renderDate = () => {
       let message = 'Created ';
       let timestamp = createdAt;
@@ -16,12 +17,16 @@ const Todo = React.createClass({
       return message + moment.unix(timestamp).format('Do MMM YYYY @ HH:mm');
     };
     return (
-      <div onClick={() => {
+      <div className={todoClassName} onClick={() => {
         this.props.onToggle(id);
       }}>
-        <input type="checkbox" checked={completed} />
-        <p>{text}</p>
-        <p>{renderDate()}</p>
+        <div>
+          <input type="checkbox" checked={completed} />
+        </div>
+        <div>
+          <p>{text}</p>
+          <p className="todo__subtext">{renderDate()}</p>
+        </div>
       </div>
     );
   }
