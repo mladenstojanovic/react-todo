26ea2f607d840dc0f355dcaed48c0749cf1e0e8d
Mladen <mladja92@yahoo.com>
2017-03-03 17:10:06 +0100

    add completed and created timestamps

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,14 +1,27 @@
 const React = require('react');
+const moment = require('moment');
 
 const Todo = React.createClass({
   render: function () {
-    const {text, id, completed} = this.props;
+    const {text, id, completed, createdAt, completedAt} = this.props;
+    const renderDate = () => {
+      let message = 'Created ';
+      let timestamp = createdAt;
+
+      if (completed) {
+        message = 'Completed ';
+        timestamp = completedAt;
+      }
+
+      return message + moment.unix(timestamp).format('Do MMM YYYY @ HH:mm');
+    };
     return (
       <div onClick={() => {
         this.props.onToggle(id);
       }}>
         <input type="checkbox" checked={completed} />
-        {text}
+        <p>{text}</p>
+        <p>{renderDate()}</p>
       </div>
     );
   }
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -19,13 +19,16 @@ describe('TodoApp', () => {
     todoApp.handleAddTodo(todoText);
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
   });
 
   it('should toggle completed value when handleToggle called', () => {
     const todoData = {
       id: 11,
       text: 'sta god bajo',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     };
     const todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
     todoApp.setState({todos: [todoData]});
@@ -34,6 +37,23 @@ describe('TodoApp', () => {
     expect(todoApp.state.todos[0].completed).toBe(false);
     todoApp.handleToggle(11);
     expect(todoApp.state.todos[0].completed).toBe(true);
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+  });
 
+  // test that when toggle from true to false, completedat gets removed
+  it('should remove completedAt when toggled completed to false', () => {
+    const todoData = {
+      id: 11,
+      text: 'sta god bajo',
+      completed: true,
+      createdAt: 0,
+      completedAt: 123
+    };
+    const todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+
+    todoApp.handleToggle(11);
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
   });
 });
